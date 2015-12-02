f58112d4fa16552ad0b80b1357ef692686f586fe
Chad Anderson <canderson@domaintools.com>
2015-12-01 16:32:08 -0800

    Whoops forgot 02.

---
+++ python/01-2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+with open('../inputs/01.txt') as f:
+    floor = 0
+    line = f.read()
+    for i, char in enumerate(line):
+        if char == '(': 
+            floor += 1
+        elif char == ')':
+            floor -= 1
+
+        if floor == -1:
+            print(i)
+            break
